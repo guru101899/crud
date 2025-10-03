ac31d9d16f53f95acdfd6747dbe0e580a1219ebf
Abdul Rehman <abdulrehmanshafy101@gmail.com>
2025-10-03 17:57:32 +0500

    add comment

--- crudproject1/enroll/models.py
+++ crudproject1/enroll/models.py
@@ -5,3 +5,5 @@ class User(models.Model):
     name=models.CharField(max_length=70)
     email=models.EmailField(max_length=100,unique=True)
     password=models.CharField(max_length=128)
+
+# just adding a comment to see the pull request
